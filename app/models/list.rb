8eae06799bdb531bce15998171df65c39eca5eab
João Lucas <jlgm@cin.ufpe.br>
2015-07-06 16:10:56 -0300

    making some changes into login/signin features

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,3 +2,15 @@ class List < ActiveRecord::Base
   belongs_to :user
   has_many :tasks
 end
+
+
+RSpec.describe Post, :type => :model do
+  context "with 2 or more comments" do
+    it "orders them in reverse chronologically" do
+      expect {
+        
+
+      }
+    end
+  end
+end
